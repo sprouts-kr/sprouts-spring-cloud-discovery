6bbe22240466f8cc6acadafb8e21a46ccd957d2b
Jinwon Seo <jwseo@live.co.kr>
2023-08-18 14:50:13 +0900

    Remove empty test method.

--- src/test/java/kr/sprouts/framework/discovery/DiscoveryApplicationTests.java
+++ src/test/java/kr/sprouts/framework/discovery/DiscoveryApplicationTests.java
@@ -1,11 +1,6 @@
 package kr.sprouts.framework.discovery;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryApplicationTests {
-
-	@Test
-	void contextLoads() { }
-}
+class DiscoveryApplicationTests { }
